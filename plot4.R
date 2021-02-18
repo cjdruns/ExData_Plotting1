f679b7188012de0d2a2fb406f9a9e8796126500f
cjdruns <77007381+cjdruns@users.noreply.github.com>
2021-02-18 12:33:56 -0500

    Added the scripts and png files of plots 1-4

---
+++ plot4.R
@@ -0,0 +1,31 @@
+#setwd('C:/R_studio/exploratory_data_analysis/courseproj1')
+
+data <- read.table('exdata_data_household_power_consumption/household_power_consumption.txt', header=T, sep = ';', na.strings = '?', stringsAsFactors = F)
+names <- names(data)
+# "Date"                  "Time"                  "Global_active_power"   "Global_reactive_power" "Voltage" "Global_intensity"      
+#"Sub_metering_1"  "Sub_metering_2"        "Sub_metering_3"       
+
+cd <- data[complete.cases(data),]
+for (i in 3:length(names(cd))){
+      
+      cd[,i]  <-as.numeric( cd[,i])
+}
+sub <- subset(cd,Date == c("1/2/2008", '2/2/2008') )
+dt <- strptime(paste(sub$Date, sub$Time, sep = ' '), '%d/%m/%Y %H:%M:%S' )
+
+par(mfrow = c(2, 2)) 
+
+plot(dt, sub$Voltage, type = 'l', xlab = 'Day', ylab = 'Voltage')
+plot(dt,sub$Global_active_power,type="l", main = 'Global Active Power',
+    xlab="Date",ylab="Global Active Power")
+clr <- c('black','red','blue')
+names <- c("Sub_metering_1",  "Sub_metering_2",  "Sub_metering_3" )
+plot(dt, sub$Sub_metering_1, type = 'n', xlab = 'Date', ylab = 'Energy Sub Metering')
+lines(dt, sub$Sub_metering_1, col= 'black')
+lines(dt, sub$Sub_metering_2, col= 'red')
+lines(dt, sub$Sub_metering_3, col = 'blue')
+legend('topleft', lty = c(1,1,1), col=clr, legend = names, cex = .5)
+plot(dt, sub$Global_reactive_power, type = 'l', xlab = 'Day', ylab = 'Global Reactive Power')
+
+dev.copy(png, file="plot4.png", width=480, height=480)
+dev.off()
